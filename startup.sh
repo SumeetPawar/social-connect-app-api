1aa639c1f5a93f20c04c90d6d8f94b6cf559f7d0
Pawar, Sumeet <Sumeet.Pawar@Honeywell.com>
2026-01-28 20:15:14 +0530

    workers kept one instead

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 echo "Running database migrations..."
 python -m alembic upgrade head
 echo "Starting application..."
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind=0.0.0.0:8000
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker app.main:app --bind=0.0.0.0:8000 --timeout 120
